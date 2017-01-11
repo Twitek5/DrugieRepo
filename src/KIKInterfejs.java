9afd9b8d2ee3397cb0ee120521fd3a4babb1bc82
unknown <you@example.com>
2017-01-11 20:35:31 +0100

    Dodano metody rysowania planszy i info o zlym polu. Dodano wywolanie w main

--- src/KIKInterfejs.java
+++ src/KIKInterfejs.java
@@ -2,33 +2,41 @@ import java.util.Scanner;
 
 public class KIKInterfejs {
 
-	
-	 public int menu() {
-	        
-	        System.out.println("1. Start");
-	        System.out.println("0. Wyniki");
-	        Scanner scanner = new Scanner(System.in);
-	        int odp = scanner.nextInt();
-	        return odp;
-	        }
-	 
-	 public String podajImie() {
-		 System.out.println("podaj swoje imie");
-		 Scanner scanner = new Scanner(System.in);
-		 String imie = scanner.nextLine();
-		 return imie;
-		 
- }
-	 
-	 public int podajPole (String imie) {
-		 
-		 System.out.println("Gracz: "+ imie);
-		 System.out.println("Podaj nr pola: ");
-		 Scanner scanner = new Scanner(System.in);
-		 int pozycja = scanner.nextInt();
-		 return pozycja;
-				 
-	 }
-	 
-	 
+	public int menu() {
+
+		System.out.println("1. Start");
+		System.out.println("0. Wyniki");
+		Scanner scanner = new Scanner(System.in);
+		int odp = scanner.nextInt();
+		return odp;
+	}
+
+	public String podajImie() {
+		System.out.println("podaj swoje imie");
+		Scanner scanner = new Scanner(System.in);
+		String imie = scanner.nextLine();
+		return imie;
+
+	}
+
+	public int podajPole(String imie) {
+
+		System.out.println("Gracz: " + imie);
+		System.out.println("Podaj nr pola: ");
+		Scanner scanner = new Scanner(System.in);
+		int pozycja = scanner.nextInt();
+		return pozycja;
+
+	}
+
+	public void pokazPlansze(Plansza plansza) {
+		System.out.println(plansza.toString());
+	}
+
+	public void podanoNiepoprawnePole() {
+		System.out.println("Podano niepoprawne pole. Sprobuj jeszcze raz");
+		Scanner scanner = new Scanner(System.in);
+		scanner.nextLine();
+	}
+
 }
